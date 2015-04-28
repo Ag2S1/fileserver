a2f8839050ebcacb5f59d797fd90c914035ab31b
Yulong Wang <wangyulong@gmail.com>
2015-04-28 11:42:44 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# FileServer
+A tool to serves files from the current directory and below, directly mapping the directory structure to HTTP requests.
+
+# Install
+	go get github.com/muhammadwang/fileserver
+
+# How to use
+	fileserver
---
+++ fileserver.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Start file server in " + dir)
+
+	handler := http.FileServer(http.Dir(dir))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
